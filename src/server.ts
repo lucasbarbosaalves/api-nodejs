ff3818569cd2c1e41001998bc3c9b2c4f1ae5880
lucas.barbosa <lkab05@hotmail.com>
2025-07-29 18:57:12 -0300

    feat: implemented Fastify app and register transaction routes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,5 @@
-import fastify from 'fastify';
-import { knex } from './database';
-import { randomUUID } from 'crypto';
+import { app } from './app';
 import { env } from './env';
-
-const app = fastify();
-
-app.get('/', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: randomUUID(),
-      title: 'Transaction',
-      amount: 1000,
-    })
-    .returning('*');
-  return transaction;
-});
 
 app
   .listen({
